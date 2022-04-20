516bb0cd62a0aaf747d7e37e0fabc9732d4ffd20
Adolfo <adolfo8799@gmail.com>
2022-04-20 11:53:06 -0300

    fix: erro

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,5 +3,7 @@ export const environment = {
   production: false,
   apiUrl: 'https://localhost:44380/api/',
   apiUrlImage: 'https://localhost:44380/Resources/Imagens',
-  secretKey: 'YourSecretKeyForEncryption&Descryption'
+  secretKey: 'YourSecretKeyForEncryption&Descryption',
+  googleProvider:'226014073143-ml8ufg6nmtjsph9o67j6t30rjjljv821.apps.googleusercontent.com',
+  facebookProvider:'1110826919739685'
 };
