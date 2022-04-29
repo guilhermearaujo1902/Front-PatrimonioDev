b25d326846ce772deba3b4c6ee3585910a5c6988
Adolfo <adolfo8799@gmail.com>
2022-04-29 09:48:41 -0300

    fix: corrige url back-end

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://patrimoniops.azurewebsites.net/api/',
-  apiUrlImage: 'https://patrimoniops.azurewebsites.net/Resources/Imagens',
+  apiUrl: 'https://patrimoniops-web.azurewebsites.net/api/',
+  apiUrlImage: 'https://patrimoniops-web.azurewebsites.net/Resources/Imagens',
   secretKey: 'YourSecretKeyForEncryption&Descryption',
   googleProvider:'226014073143-ml8ufg6nmtjsph9o67j6t30rjjljv821.apps.googleusercontent.com',
   facebookProvider:'1110826919739685'
