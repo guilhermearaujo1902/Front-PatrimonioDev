000aa9380a03772602046de5f0efe430f4a081d3
Adolfo <adolfo8799@gmail.com>
2022-07-19 21:05:59 -0300

    fix: troca url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://patrimoniodevpsweb.azurewebsites.net/api/',
-  apiUrlImage: 'https://patrimoniodevpsweb.azurewebsites.net/Resources/Imagens',
+  apiUrl: 'https://patrimoniodev-final.azurewebsites.net/api/',
+  apiUrlImage: 'https://patrimoniodev-final.azurewebsites.net/Resources/Imagens',
   secretKey: 'YourSecretKeyForEncryption&Descryption',
   googleProvider:'226014073143-ml8ufg6nmtjsph9o67j6t30rjjljv821.apps.googleusercontent.com',
   facebookProvider:'1110826919739685'
